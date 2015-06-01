7542b41af07e723dd976ed40b6ea534f5f6be8b1
Aditi Ramachandran <aditi@luminosity.wireless.yale.internal>
2015-06-01 15:13:57 -0400

    changed StartActivity

--- src/com/priyanka/StartActivity.java
+++ src/com/priyanka/StartActivity.java
@@ -1,5 +1,31 @@
+package com.priyanka;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 /**
  * Created by aditi on 5/29/15.
  */
-public class StartActivity {
+public class StartActivity extends Activity {
+    private EditText IPandPort;
+    private Button Session1Button;
+    private Button Session2Button;
+    private Button Session3Button;
+    private Button Session4Button;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.start_screen);
+
+        IPandPort = (EditText) findViewById(R.id.IPandPort);
+        Session1Button = (Button) findViewById(R.id.Session1Button);
+        Session2Button = (Button) findViewById(R.id.Session2Button);
+        Session3Button = (Button) findViewById(R.id.Session3Button);
+        Session4Button = (Button) findViewById(R.id.Session4Button);
+
+
+    }
 }
