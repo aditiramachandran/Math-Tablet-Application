a41f284496caf44d95f34572858506d0acaf0ed2
Aditi Ramachandran <aditi@luminosity.local>
2015-06-02 17:43:37 -0400

    Added tcp stuff to MainActivity

--- src/com/priyanka/MainActivity.java
+++ src/com/priyanka/MainActivity.java
@@ -1,10 +1,22 @@
 package com.priyanka;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 import com.priyanka.TCPClient;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Log;
+import android.os.AsyncTask;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -30,10 +42,7 @@ public class MainActivity extends Activity {
 
         IPandPort = (EditText) findViewById(R.id.IPandPort);
         ConnectButton = (Button) findViewById(R.id.ConnectButton);
-
-
-
-
+        ConnectionStatus = (TextView) findViewById(R.id.ConnectionStatus);
 
         Session1Button = (Button) findViewById(R.id.Session1Button);
         Session2Button = (Button) findViewById(R.id.Session2Button);
@@ -49,10 +58,112 @@ public class MainActivity extends Activity {
     }
 
     public void connectTablet(View view){
-        String ipaddress = IPandPort.getText().toString();
+        String ipInput = IPandPort.getText().toString();
+        String ipaddress = ipInput.split(":")[0];
+        new connectTask().execute(ipaddress);
 
-        //new connectTask().execute(ipaddress);
+        ConnectionStatus.setText("Trying to connect to server");
+    }
 
+    public class connectTask extends AsyncTask<String,String,TCPClient> {
 
+        private String ipaddress;
+        @Override
+        protected TCPClient doInBackground(String... message) {
+
+            //we create a TCPClient object and
+            mTcpClient = new TCPClient(new TCPClient.OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                    onProgressUpdate(message);
+
+                    Log.e("MainActivity", "Message received from server: hiding options");
+
+                    // showMoodMeter = false;
+                    //if (message.equalsIgnoreCase("showOptions"))
+                    // aditi showOptions = true;
+                    // aditi currentlyPlaying = new String("");
+	                    /*thread.setRunning(false);
+	    				((Activity)getContext()).finish();*/
+
+                }
+            });
+
+            if (this.validIP(message[0])){
+                mTcpClient.setIpAddress(message[0]);
+
+                //if valid, write ip in text file
+                BufferedWriter writer = null;
+                try
+                {
+                    writer = new BufferedWriter(new FileWriter("/sdcard/Movies/ip.txt"));
+                    writer.write(message[0]);
+                }
+                catch (IOException e)
+                {
+                }
+                finally
+                {
+                    try
+                    {
+                        if ( writer != null)
+                            writer.close( );
+                    }
+                    catch ( IOException e)
+                    {
+                    }
+                }
+
+            } else {  //if not valid IP, try to read the one from the text file
+                String ipaddress = null;
+                BufferedReader br = null;
+                try {
+                    br = new BufferedReader(new FileReader("/sdcard/Movies/ip.txt"));
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                try {
+                    String savedIP = br.readLine();
+                    br.close();
+                    if (this.validIP(savedIP))
+                        mTcpClient.setIpAddress(savedIP);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            // mTcpClient.setIpAddress(message[0]);
+            mTcpClient.run();
+
+            return null;
+        }
+
+        public boolean validIP(String ip) {
+            if (ip == null || ip.isEmpty()) return false;
+            ip = ip.trim();
+            if ((ip.length() < 6) & (ip.length() > 15)) return false;
+
+            try {
+                Pattern pattern = Pattern.compile("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+                Matcher matcher = pattern.matcher(ip);
+                return matcher.matches();
+            } catch (PatternSyntaxException ex) {
+                return false;
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            //in the arrayList we add the messaged received from server
+            // arrayList.add(values[0]);
+            // notify the adapter that the data set has changed. This means that new message received
+            // from server was added to the list
+            //mAdapter.notifyDataSetChanged();
+        }
     }
 }
--- src/com/priyanka/TCPClient.java
+++ src/com/priyanka/TCPClient.java
@@ -1,5 +1,131 @@
 /**
  * Created by aditi on 6/2/15.
  */
+package com.priyanka;
+
+import android.util.Log;
+
+import android.util.Log;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class TCPClient {
+
+    private String serverMessage;
+    public static final String SERVERIP = "192.168.42.109";  //192.168.1.106"; //your computer IP address
+    public static final int SERVERPORT = 6789;
+    private OnMessageReceived mMessageListener = null;
+    private boolean mRun = false;
+    private String ipAddressVar;
+
+    PrintWriter out;
+    BufferedReader in;
+
+    /**
+     *  Constructor of the class. OnMessagedReceived listens for the messages received from server
+     */
+    public TCPClient(OnMessageReceived listener) {
+        mMessageListener = listener;
+        ipAddressVar = null;
+    }
+
+    /**
+     * Sends the message entered by client to the server
+     * @param message text entered by client
+     */
+    public void sendMessage(String message){
+        if (out != null && !out.checkError()) {
+            out.println(message);
+            out.flush();
+        }
+    }
+
+    public void stopClient(){
+        mRun = false;
+    }
+
+    public void run() {
+
+        mRun = true;
+
+        try {
+            //here you must put your computer's IP address.
+            InetAddress serverAddr;
+
+            if (ipAddressVar == null)
+                serverAddr = InetAddress.getByName(SERVERIP);
+            else
+                serverAddr = InetAddress.getByName(ipAddressVar);
+
+            Log.e("TCP Client", "C: Connecting...");
+
+            //create a socket to make the connection with the server
+            Socket socket = new Socket(serverAddr, SERVERPORT);
+
+            try {
+
+                //send the message to the server
+                out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+                out.print("Session started!\r\n");
+                out.flush();
+
+                Log.e("TCP Client", "C: Sent.");
+
+                Log.e("TCP Client", "C: Done.");
+
+                //receive the message which the server sends back
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+
+                //in this while the client listens for the messages sent by the server
+                while (mRun) {
+                    Log.e("TCP Client", "Running");
+                    serverMessage = in.readLine();
+                    Log.e("TCP Client", "Receiving" + in);
+
+                    if (serverMessage != null && mMessageListener != null) {
+                        //call the method messageReceived from MyActivity class
+                        mMessageListener.messageReceived(serverMessage);
+                    }
+                    serverMessage = null;
+
+                }
+
+
+                Log.e("RESPONSE FROM SERVER", "S: Received Message: '" + serverMessage + "'");
+
+
+            } catch (Exception e) {
+
+                Log.e("TCP", "S: Error", e);
+
+            } finally {
+                //the socket must be closed. It is not possible to reconnect to this socket
+                // after it is closed, which means a new socket instance has to be created.
+                socket.close();
+            }
+
+        } catch (Exception e) {
+
+            Log.e("TCP", "C: Error", e);
+
+        }
+
+    }
+
+    public String getIpAddress() {
+        return ipAddressVar;
+    }
+
+    public void setIpAddress(String ipAddressVar) {
+        this.ipAddressVar = ipAddressVar;
+    }
+
+    //Declare the interface. The method messageReceived(String message) will must be implemented in the MyActivity
+    //class at on asynckTask doInBackground
+    public interface OnMessageReceived {
+        public void messageReceived(String message);
+    }
 }
