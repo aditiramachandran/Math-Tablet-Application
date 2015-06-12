553bcbd0a0ae87f73570b338ba22439dbe121802
Aditi Ramachandran <aditi@luminosity.wireless.yale.internal>
2015-06-12 16:10:16 -0400

    added radio buttons for experimental conditions and passed that to MathActivity

--- python_scripts/nao_server.py
+++ python_scripts/nao_server.py
@@ -97,6 +97,13 @@ class TutoringSession:
 						os.system("say " + robot_speech)
 					else:
 						self.goNao.assess("wrong")
+				elif msgType == 'LIA': #incorrect attempt
+					self.numIncorrect += 1
+					print 'incorrect answer (last attempt)'
+					if self.goNao is None:
+						os.system("say " + robot_speech)
+					else:
+						self.goNao.genSpeech(robot_speech)		
 				elif msgType == 'H1': #hint request
 					self.numHintRequests += 1
 					print 'hint 1 request'
@@ -123,6 +130,10 @@ class TutoringSession:
 					print 'repeat hint request'
 				elif msgType == 'END': #session ended
 					print 'tutoring session ended'
+					if self.goNao is None:
+						os.system("say " + robot_speech)
+					else:
+						self.goNao.genSpeech(robot_speech)	
 					break
 				else:
 					print 'error: unknown message type'
@@ -263,7 +274,7 @@ def main():
 		#Set all the possible commands
 		commands=collections.OrderedDict((("i","Run the intro"),
 		("r","Release motors"),
-		("t","Test new code"),
+		("t","Type something for the nao to say"),
 		("m","Move nao head - test"),
 		("s","Start tutoring interaction"),
 		("q", "Quit"),
@@ -291,8 +302,10 @@ def main():
 		    goNao.releaseNao()
 
 		elif(choice == "t"):
-			history = open("data/Tony.txt","a")
-			tutor(history)
+			phrase = raw_input('Type phrase here: ')
+			goNao.genSpeech(phrase)
+			#history = open("data/Tony.txt","a")
+			#tutor(history)
 
 		elif(choice == "m"):
 			goNao.move_head()
