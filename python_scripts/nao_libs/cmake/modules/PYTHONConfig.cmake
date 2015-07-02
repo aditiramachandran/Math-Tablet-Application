e9fa4a8e1a574d61e3dacb6c97653564e5e399e0
aditiramachandran <aditiramachandran@gmail.com>
2015-07-02 14:34:08 -0400

    Removed nao_libs files

--- python_scripts/nao_libs/cmake/modules/PYTHONConfig.cmake
+++
@@ -1,8 +0,0 @@
-## Copyright (C) 2008 Aldebaran Robotics
-
-clean(PYTHON)
-fpath(PYTHON Python.h PATH_SUFFIXES "python2.6")
-flib(PYTHON OPTIMIZED NAMES python26   python2.6 Python)
-flib(PYTHON DEBUG     NAMES python26_d python2.6 Python)
-export_lib(PYTHON)
-
--- python_scripts/nao_libs/cmake/modules/QPOASESConfig.cmake
+++
@@ -1,7 +0,0 @@
-include("${TOOLCHAIN_DIR}/cmake/libfind.cmake")
-
-clean(QPOASES)
-
-fpath(QPOASES QProblem.hpp PATH_SUFFIXES qpoases)
-flib(QPOASES qpoases)
-export_lib(QPOASES)
