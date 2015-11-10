708aa8018b527a920c63d12f138ae94d317211de
Eric Ho <eric.ho@yale.edu>
2015-11-10 03:06:42 -0500

    added silhouette graph

--- python_scripts/data_analysis/k_means_denied_auto.m
+++ python_scripts/data_analysis/k_means_denied_auto.m
@@ -3,7 +3,7 @@
 
 % denied_auto_hints.csv is the output of denied_auto_hints_extraction.py
 % contains aggregate denied/auto hints across sessions for each participant
-X = csvread('denied_auto_hints.csv');
+X = csvread('denied_auto_hints_session1.csv');
 
 % comment this section out if we do not want to use the means of both
 % features to normalize the matrix
@@ -15,20 +15,30 @@ X = csvread('denied_auto_hints.csv');
 % idx is a vector of assigned cluster IDs, C contains the positions of the
 % centroids, and sumd contains the summed distances of points to the
 % centroids
-[idx, C, sumd] = kmeans(X, 4);
+[idx, C, sumd] = kmeans(X, 3, 'Distance', 'sqEuclidean', 'Display', 'final', 'Replicates', 5);
 figure;
 % plots clusters with different colors
 plot(X(idx==1,1),X(idx==1,2),'r.','MarkerSize',25)
 hold on
 plot(X(idx==2,1),X(idx==2,2),'b.','MarkerSize',25)
 plot(X(idx==3,1),X(idx==3,2),'g.','MarkerSize',25)
-plot(X(idx==4,1),X(idx==4,2),'c.','MarkerSize',25)
+% plot(X(idx==4,1),X(idx==4,2),'c.','MarkerSize',25)
 
 % plots centroids
 plot(C(:,1),C(:,2),'kx',...
      'MarkerSize',15,'LineWidth',3)
-legend('Cluster 1','Cluster 2','Cluster 3', 'Cluster 4','Centroids')
+legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Centroids')
 title 'Denied/Auto hints kmeans'
 xlabel 'Denied Hints'
 ylabel 'Auto Hints'
 hold off
+
+figure;
+[silh,h] = silhouette(X, idx);
+h = gca;
+h.Children.EdgeColor = [.8 .8 1];
+xlabel 'Silhouette Value';
+ylabel 'Cluster';
+
+% calculates accuracy of silhouette model
+c = mean(silh);
